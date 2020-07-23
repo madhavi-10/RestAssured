ad058f270fe86caca976e31a5412c759d5e57902
madhavi.dixit <madhavidixit.23@gmail.com>
2020-07-23 11:12:30 +0530

    comitt

---
+++ RestAPIAutomation/src/main/java/RestAPIAutomation/RestAPIAutomation/App.java
@@ -0,0 +1,13 @@
+package RestAPIAutomation.RestAPIAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
